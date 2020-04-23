61762b178121f06a09ac7636e3b39c27a42291e4
ZeyuKeithFu <zeyufu@bu.edu>
2020-04-23 15:46:43 -0400

    Fix new line.

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-exec & /upspin/upspinserver -tls_cert /upspin/cert/upspin-openshift-infrastructure-as-code.k-apps.osh.massopen.cloud.crt -tls_key /upspin/cert/upspin-openshift-infras
-tructure-as-code.k-apps.osh.massopen.cloud.key
+exec & /upspin/upspinserver -tls_cert /upspin/cert/upspin-openshift-infrastructure-as-code.k-apps.osh.massopen.cloud.crt -tls_key /upspin/cert/upspin-openshift-infrastructure-as-code.k-apps.osh.massopen.cloud.key
