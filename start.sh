6d2c6c4892cc19726ab8e09e36086811637175ed
ZeyuKeithFu <zeyufu@bu.edu>
2020-04-28 20:20:57 -0400

    Remove .pem

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-exec & /upspin/upspinserver -tls_cert /upspin/cert/server.crt.pem -tls_key /upspin/cert/server.key.pem
+exec & /upspin/upspinserver -tls_cert /upspin/cert/server.crt -tls_key /upspin/cert/server.key
