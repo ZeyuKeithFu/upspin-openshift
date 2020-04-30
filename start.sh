ba82d8deb5c6128afba29dad2a42ab6e5cac066a
ZeyuKeithFu <zeyufu@bu.edu>
2020-04-29 21:18:50 -0400

    Recover .pem ext

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-exec & /upspin/upspinserver -tls_cert /upspin/cert/server.crt -tls_key /upspin/cert/server.key
+exec & /upspin/upspinserver -tls_key /upspin/cert/server.key.pem -tls_cert /upspin/cert/server.crt.pem
