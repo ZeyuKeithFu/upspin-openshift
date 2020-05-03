049fb1ae0b315f5721e8077101a1655feabb950b
ZeyuKeithFu <zeyufu@bu.edu>
2020-05-03 00:38:41 -0400

    Change user.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-exec & /home/upspin/upspinserver -tls_key /home/upspin/cert/upspin.k-apps.osh.massopen.cloud.key -tls_cert /home/upspin/cert/upspin.k-apps.osh.massopen.cloud.crt.pem
+exec --user upspin & /home/upspin/upspinserver -tls_key /home/upspin/cert/upspin.k-apps.osh.massopen.cloud.key -tls_cert /home/upspin/cert/upspin.k-apps.osh.massopen.cloud.crt.pem
